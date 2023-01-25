c2469ae8edbdb2a72136dede507635522b7f2da9
a22javiermm <a22javiermm@192.168.56.1>
2023-01-25 10:19:02 +0100

    archivo2

--- Tetris/src/tetris/Xogo.java
+++ Tetris/src/tetris/Xogo.java
@@ -1,13 +1,51 @@
 package tetris;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author a22javiermm
  */
 public class Xogo {
-    final int Lado_cadrado=2;
+
+    final int LADO_CADRADO= 2;
     final int MAX_Y=20;
     final int MAX_X=20;
     boolean pausa;
-    int numeroLinas;
+    int numeroLinas=MAX_Y-LADO_CADRADO;
+    
+    ArrayList<Cadrado>cadradosChan=new ArrayList<Cadrado>();
+
+    public void moverFichaDereita() {
+
+    }
+
+    public void moverFichaEsquerda() {
+
+    }
+
+    public void moverFichaAbaixo() {
+
+    }
+
+    public void rotarFicha() {
+
+    }
+
+    public boolean ePosicionValida(int x, int y) {
+        if (x > MAX_X || y > MAX_Y) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    public void engadirFichaAoChan(){
+        
+    }
+    
+    public boolean chocaFichaCoChan(){
+        getCoordenadas();
+        if(ficha)
+    }
 }
